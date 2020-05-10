50d1cdc1b460ab716ea2c14033ea042cf99eb251
GloriousEggroll <gloriouseggroll@gmail.com>
2020-05-09 20:27:37 -0600

    patches: update non-mf patches to to match mf branch

--- patches/protonprep.sh
+++ patches/protonprep.sh
@@ -60,12 +60,14 @@
     git clean -xdf
     patch -Np1 < ../patches/dxvk/valve-dxvk-avoid-spamming-log-with-requests-for-IWineD3D11Texture2D.patch
     patch -Np1 < ../patches/dxvk/proton-add_new_dxvk_config_library.patch
+    patch -Np1 < ../patches/dxvk/dxvk-async.patch
     cd ..
 
     #WINE STAGING
     cd wine-staging
     git reset --hard HEAD
     git clean -xdf
+    
 
     # fixes patching without rawinput
     patch -Np1 < ../patches/wine-hotfixes/staging-44d1a45-localreverts.patch    
@@ -111,8 +113,8 @@
 
 
     #WINE FAUDIO
-    echo "applying faudio patches"
-    patch -Np1 < ../patches/faudio/faudio-ffmpeg.patch
+    #echo "applying faudio patches"
+    #patch -Np1 < ../patches/faudio/faudio-ffmpeg.patch
 
 
     ### GAME PATCH SECTION ###
@@ -144,12 +146,6 @@
 #   TODO: Add game-specific check
     echo "NFSW launcher fix"
     patch -Np1 < ../patches/game-patches/NFSWLauncherfix.patch
-
-#   TODO: Check on this - don't own game. Need to validate.
-#   TODO: Add game-specific check
-    echo "gta4 input patch"
-    patch -Np1 < ../patches/game-patches/gta4_gamepad_workaround.patch
-
 
 # Currently applied but not working:
 
@@ -183,6 +179,9 @@
     
     echo "fullscreen hack"
     patch -Np1 < ../patches/proton/valve_proton_fullscreen_hack-staging.patch
+    
+    echo "fix for Dark Souls III, Sekiro, Nier" 
+    patch -Np1 < ../patches/game-patches/winex11_limit_resources-nmode.patch
 
     echo "raw input"
     patch -Np1 < ../patches/proton/proton-rawinput.patch
@@ -263,13 +262,11 @@
     echo "proton MF hacks"
     patch -Np1 < ../patches/proton/proton-mf_hacks.patch
 
-    # WINE HOTFIXES
-    patch -Np1 < ../patches/wine-hotfixes/server-fix_setting_context_flags_in_get_thread_context.patch
-
     #WINE CUSTOM PATCHES
     #add your own custom patch lines below
     patch -Np1 < ../patches/wine-hotfixes/user32-Set_PAINTSTRUCT_fErase_field_depending_on_the_last_WM_ERASEBKGND_result.patch
-    patch -Np1 < ../patches/wine-hotfixes/ntdll-Use_the_free_ranges_in_find_reserved_free_area.patch
+#    patch -Np1 < ../patches/wine-hotfixes/ntdll-Use_the_free_ranges_in_find_reserved_free_area.patch
+    patch -Np1 < ../patches/wine-hotfixes/dll_loader_fix.patch
 
     ./dlls/winevulkan/make_vulkan
     ./tools/make_requests
